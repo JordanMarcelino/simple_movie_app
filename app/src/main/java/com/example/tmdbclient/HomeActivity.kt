252d8213cd36b17bdb4f501db13cb4ccdd47e517
JordanMarcelino <jordangamers25@gmail.com>
2022-02-09 23:24:12 +0700

    Making home screen layout

--- app/src/main/java/com/example/tmdbclient/HomeActivity.kt
+++ app/src/main/java/com/example/tmdbclient/HomeActivity.kt
@@ -3,15 +3,15 @@ package com.example.tmdbclient
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.example.tmdbclient.databinding.ActivityMainBinding
+import com.example.tmdbclient.databinding.ActivityHomeBinding
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
 
     }
 }
